4f72464aad3a12e38c6b160549c6a86b9a25b3ea
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-11-06 14:26:59 +0100

    Initial commit

---
+++ vm.tf
@@ -0,0 +1,27 @@
+# Create Google Cloud VM | vm.tf
+
+# Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 4
+}
+
+# Create VM #1
+resource "google_compute_instance" "vm_instance_1" {
+  name         = "${var.app_name}-vm-${random_id.instance_id.hex}"
+  machine_type = "f1-micro"
+  zone         = var.gcp_zone_a
+  hostname     = "${var.app_name}-vm-${random_id.instance_id.hex}.${var.app_domain}"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential apache2"
+
+  network_interface {
+    network       = "${var.app_name}-vpc"
+    access_config { }
+  }
+} 
