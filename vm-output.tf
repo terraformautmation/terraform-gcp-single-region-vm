62b0aa82f665c89d1a2c1753878bf05e567b3967
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-11-06 14:34:33 +0100

    Initial commit

---
+++ vm-output.tf
@@ -0,0 +1,8 @@
+# Output VM1
+output "instance_id_vm1" {
+  value = "${google_compute_instance.vm_instance_1.self_link}"
+}
+output "ip-vm1" {
+ value = "${google_compute_instance.vm_instance_1.network_interface.0.access_config.0.nat_ip}"
+}
+
