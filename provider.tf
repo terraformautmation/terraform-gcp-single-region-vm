c462d7d45d309823f6b76355a03ee00d4464b6f4
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-11-06 16:17:30 +0100

    Initial commit

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 provider "google" {
-  project = "kopicloud"
+  project = var.app_project
   credentials = file(var.gcp_auth_file)
   region  = var.gcp_region_1
   zone    = var.gcp_zone_1
